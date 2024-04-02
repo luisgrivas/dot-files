5e9b2a1d6bc27017bc8650379a10e53ac1ede641
luisgrivas <lufegori93@gmail.com>
2024-04-02 10:04:42 -0600

    hecho v1 de config neovim

---
+++ nvim/lua/lufeg/plugins/telescope-file-browser.lua
@@ -0,0 +1,8 @@
+return {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+    vim.keymap.set("n", "<space>t", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
+    end,
+}
