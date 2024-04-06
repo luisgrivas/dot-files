5e7fba7cc916dbbc4a765cd9e84542deb1dd1be4
luisgrivas <lufegori93@gmail.com>
2024-04-06 10:27:32 -0600

    eliminado version anterior config nvim

--- nvim/lua/lufeg/plugins/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-    vim.keymap.set("n", "<space>t", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-
-    end,
-}
