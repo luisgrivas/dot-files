e144a622e56f2a235f70e8440f1eeacbb20a2958
Luis Gonzalez <lufegori93@gmail.com>
2025-11-18 12:00:22 -0600

    Actualizar cmp.lua

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -44,7 +44,7 @@ return {
 			},
 			formatting = {
 				format = lspkind.cmp_format({
-					mode = "symbol",
+					mode = "text",
 					maxwidth = 70,
 					ellipsis_char = "...",
 					show_labelDetails = true,
